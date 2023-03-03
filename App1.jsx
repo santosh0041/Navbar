ca045beabb209ef1681b53c2b043999fe4ff79d8
santosh0041 <126310880+santosh0041@users.noreply.github.com>
2023-03-03 20:37:03 +0530

    Add files via upload

---
+++ App1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav1 from "./components/Nav1";
+import "./global.css"
+
+const App1= ()=>{
+    return(
+
+        <>
+        <Nav1/>
+        </>
+        
+    )
+}
+export default App1;
---
+++ index.js
@@ -0,0 +1,27 @@
+ import React from "react";
+ import {createRoot} from "react-dom/client";
+// import App from "./App";
+// import Navbar from "./Navbar";
+// import Searchbar from "./Searchbar";
+// import Logo from "./Logo";
+// import Login from "./Login";
+// import Profile from "./Profile";
+// import Account from "./Account";
+// import Movies from "./Movies";
+// import Recent from "./Recent";
+// import Most from "./Most";
+    import App1 from "./App1";
+
+
+// createRoot(document.getElementById("root")).render(<App/>)
+// createRoot(document.getElementById("Nav")).render(<Navbar/>)
+// createRoot(document.getElementById("Search")).render(<Searchbar/>)
+// createRoot(document.getElementById("Log")).render(<Logo/>)
+// createRoot(document.getElementById("Log1")).render(<Login/>)
+// createRoot(document.getElementById("Pro")).render(<Profile/>)
+// createRoot(document.getElementById("Acc")).render(<Account/>)
+// createRoot(document.getElementById("Mov")).render(<Movies/>)
+// createRoot(document.getElementById("Rec")).render(<Recent/>)
+// createRoot(document.getElementById("Mos")).render(<Most/>)
+createRoot(document.getElementById("Ap")).render(<App1/>)
+
